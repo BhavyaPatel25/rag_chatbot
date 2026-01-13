1a27653bd1ddda8df02aca147018927109867bb8
Bhavya Patel <bhavyapatel1000@gmail.com>
2026-01-13 16:24:48 -0500

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-from retriever import retrieve_context
-
-question = "What is Bhavya Patel's experience?"
-context = retrieve_context(question)
-
-print("Retrieved context:")
-print("-" * 40)
-print(context)
